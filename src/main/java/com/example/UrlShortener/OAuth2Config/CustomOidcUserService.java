7d24a3fe11e0197c70ea32a874944b89bb10cb92
Pratik Joseph Dabre <pdabre12@gmail.com>
2023-03-04 19:28:46 -0800

    Added oid custom user logic

---
+++ src/main/java/com/example/UrlShortener/OAuth2Config/CustomOidcUserService.java
@@ -0,0 +1,46 @@
+package com.example.UrlShortener.OAuth2Config;
+
+import com.example.UrlShortener.User.User;
+import com.example.UrlShortener.User.UserRepository;
+import com.example.UrlShortener.User.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class CustomOidcUserService extends OidcUserService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+
+        try {
+            return processOidcUser(userRequest, oidcUser);
+        } catch (Exception ex) {
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    private OidcUser processOidcUser(OidcUserRequest userRequest, OidcUser oidcUser) {
+        OAuth2ClientUserInfo userInfo = new OAuth2ClientUserInfo(oidcUser.getAttributes());
+        Optional<User> userOptional = userService.findUserByEmail(userInfo.getEmail());
+        if (!userOptional.isPresent()) {
+            User user = new User();
+            user.setEmail(userInfo.getEmail());
+            // set other needed data
+            userService.createUser(user);
+        }
+
+        return oidcUser;
+    }
+}
--- src/main/java/com/example/UrlShortener/config/SecurityConfiguration.java
+++ src/main/java/com/example/UrlShortener/config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.example.UrlShortener.config;
 
 
+import com.example.UrlShortener.OAuth2Config.CustomOidcUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,20 +15,32 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfiguration {
 
+    @Autowired
+    private CustomOidcUserService customOidcUserService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+//        http.csrf().disable()
+//                .authorizeHttpRequests((authz) -> authz
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(withDefaults());
+//        http.oauth2Login()
+//                .defaultSuccessUrl("http://localhost:5050/urls/", true);
+//
+//        return http.build();
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and().
+                csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .oidcUserService(customOidcUserService))
+                .defaultSuccessUrl("http://localhost:5050/urls/", true));
         return http.build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/*");
-
-    }
 
 }
